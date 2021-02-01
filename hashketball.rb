d8285a14cdfd71f310c93ffa57508321cd43b1fa
Max Miller <maxmiller413@gmail.com>
2021-02-01 04:51:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -278,16 +278,19 @@ def big_shoe_rebounds
   # end
   # rebounds 
   
+  largest_shoe_size = nil
+  rebounds = nil 
   game_hash.each do |location, team|
     team[:players].each do |player|
       player[:shoe]
-      
-        shoe
-      binding.pry 
-      
-        
-      end 
-    end 
+        if largest_shoe_size == nil 
+          largest_shoe_size = player[:shoe]
+        elsif largest_shoe_size < player[:shoe]
+          largest_shoe_size = player[:shoe]
+          rebounds = player[:rebounds]
+        end 
+    end
+    rebounds 
   end   
 end   
 
